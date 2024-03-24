c66fd6cb65221f7a2592b49fd1f6ffa001998dfd
Angelinamathew <angelinamathew707@gmail.com>
2024-03-24 14:08:52 -0400

    added test cases except copy and sort

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -3,5 +3,42 @@ package com.zipcodewilmington.singlylinkedlist;
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
+public class SinglyLinkedList<T> {
+    class Node{
+        T data;
+        Node next;
+
+        public Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    Node head = null;
+    Node tail;
+
+
+    public T find(T str2) {
+    }
+
+    public int size() {
+    }
+
+    //Represent a node of the singly linked list
+
+
+    public void add(T add) {
+        Node node = new Node(add);
+
+    }
+
+    public int get(int i) {
+    }
+
+    public void remove(int i) {
+    }
+
+    public boolean contains(T s) {
+    }
+
+
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,92 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+    @Test
+    public void testSinglyLinkedConstructor(){
+        SinglyLinkedList<String> list = new SinglyLinkedList<>();
+        Assert.assertNotNull(list);
+    }
+    @Test
+    public void testAddNode(){
+        SinglyLinkedList<String> list = new SinglyLinkedList<>();
+        String str1 = "Hello";
+        String str2 = "World";
+        String str3 = "!";
+
+        list.add(str1);
+        list.add(str2);
+        list.add(str3);
+        Assert.assertEquals("Hello", list.get(0));
+    }
+    @Test
+    public void testRemoveNode(){
+        SinglyLinkedList<String> list = new SinglyLinkedList<>();
+        String str1 = "Hello";
+        String str2 = "World";
+        String str3 = "!";
+
+        list.add(str1);
+        list.add(str2);
+        list.add(str3);
+        list.remove(2);
+        Assert.assertFalse(list.contains(str3));
+    }
+    @Test
+    public void testContainsNode(){
+        SinglyLinkedList<String> list = new SinglyLinkedList<>();
+        String str1 = "Hello";
+        String str2 = "World";
+        String str3 = "!";
+
+        list.add(str1);
+        list.add(str2);
+        list.add(str3);
+        Assert.assertFalse(list.contains(str2));
+    }
+    @Test
+    public void testFindNode(){
+        SinglyLinkedList<String> list = new SinglyLinkedList<>();
+        String str1 = "Hello";
+        String str2 = "World";
+        String str3 = "!";
+
+        list.add(str1);
+        list.add(str2);
+        list.add(str3);
+        Assert.assertEquals(1, list.find(str2));
+    }
+    @Test
+    public void testSizeNode(){
+        SinglyLinkedList<String> list = new SinglyLinkedList<>();
+        String str1 = "Hello";
+        String str2 = "World";
+        String str3 = "!";
+
+        list.add(str1);
+        list.add(str2);
+        list.add(str3);
+        Assert.assertEquals(3, list.size());
+    }
+    @Test
+    public void testGetNode(){
+        SinglyLinkedList<String> list = new SinglyLinkedList<>();
+        String str1 = "Hello";
+        String str2 = "World";
+        String str3 = "!";
+
+        list.add(str1);
+        list.add(str2);
+        list.add(str3);
+        Assert.assertEquals("!", list.get(2));
+    }
+
+
+
 }
+
